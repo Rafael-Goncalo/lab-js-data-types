bc1f888e2bf771164c11894a5f8a59fa7328fbd5
Rafael G <rafaelgoncalo@icloud.com>
2025-02-19 15:11:28 +0100

    solved lab

--- index.js
+++ index.js
@@ -9,11 +9,12 @@ const s5 = "and";
 
 // Concatenate the string variables into one new string
 
+const tongueTwister = s1 + s2 + s3 + s4 + s5
 
 // Print out the concatenated string
 
-
-
+// console.log(tongueTwister);
+//console.log(`${s1} ${s2} ${s3} ${s4} ${s5}`);
 
 /*******************************************
     Iteration 1.2 | Camel Tail
@@ -22,24 +23,35 @@ const part1 = "java";
 const part2 = "script";
 
 // Convert the last letter of part1 and part2 to uppercase and concatenate the strings
+// Sliced the last letter and turn into uppercase.
+const cameLtaiL1 = part1.slice(-1).toLocaleUpperCase();
+const cameLtaiL2 = part2.slice(-1).toLocaleUpperCase();
 
 
+// to remove the last letter from java and script
+const cameLtaiLparT1 = part1.slice(0, -1) + cameLtaiL1;
+const cameLtaiLparT2 = part2.slice(0, -1) + cameLtaiL2;
+
+// to put them togerther
+const result = cameLtaiLparT1 + cameLtaiLparT2;
 // Print the cameLtaiL-formatted string
 
-
-
+//console.log(cameLtaiL1 + cameLtaiL2); result AT 
+//console.log(cameLtaiLparT1 + cameLtaiLparT2); result javAjavT
+//console.log(result);
 
 /*******************************************
     Iteration 2.1 | Calculate Tip
 *******************************************/
 const billTotal = 84;
 
-// Calculate the tip (15% of the bill total)
 
+// Calculate the tip (15% of the bill total)
+const tipAmount = billTotal * 0.15;
 
 // Print out the tipAmount
 
-
+//console.log(tipAmount);
 
 
 /*******************************************
@@ -47,11 +59,11 @@ const billTotal = 84;
 *******************************************/
 
 // Generate a random integer between 1 and 10 (inclusive)
-
+const randomNumber = Math.ceil(Math.random() * 10)
 
 // Print the generated random number
 
-
+console.log(randomNumber)
 
 /*******************************************
     Iteration 3.1 | Booleans
